5f6fe0fdc0206c5c1abadbbed3898e4bd5136c79
Piotr Yartsev <piotr_1997@live.se>
2022-09-26 14:02:10 +0200

    made even more general

--- imagerestoration.py
+++ imagerestoration.py
@@ -18,6 +18,7 @@ im = np.array(Image.open(r"grayscale.jpg").convert('L'))
 ar = np.array(im)
 
 
+#Fixed mask
 """
 #create a mask with 1's containing a square of 0's
 mask = np.ones((1000, 1000))
@@ -37,101 +38,110 @@ ruined.show()
 ruined2=ruined.convert("L")
 ruined2.save("ruined.png")
 """
+def add_mask_of_text(text,ar,number):
 
-#Create a mask of 1
-whiteblankimage = np.ones((1000, 1000))
-#Add text with color 0 to the image
-cv2.putText(whiteblankimage, text='Piotr', org=(300,150),
-            fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
-            thickness=15, lineType=cv2.LINE_AA)
-cv2.putText(whiteblankimage, text='AND', org=(300,350),
-            fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
-            thickness=15, lineType=cv2.LINE_AA)
-cv2.putText(whiteblankimage, text='Max', org=(300,550),
-            fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
-            thickness=15, lineType=cv2.LINE_AA)
-cv2.putText(whiteblankimage, text='Fix', org=(300,750),
-            fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
-            thickness=15, lineType=cv2.LINE_AA)
-cv2.putText(whiteblankimage, text='Images', org=(300,950),
-            fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
-            thickness=15, lineType=cv2.LINE_AA)
+    #Create a mask of 1
+    whiteblankimage = np.ones((1000, 1000))
+    #Add text with color 0 to the image
+    cv2.putText(whiteblankimage, text='Piotr', org=(300,150),
+                fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
+                thickness=15, lineType=cv2.LINE_AA)
+    cv2.putText(whiteblankimage, text='AND', org=(300,350),
+                fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
+                thickness=15, lineType=cv2.LINE_AA)
+    cv2.putText(whiteblankimage, text='Max', org=(300,550),
+                fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
+                thickness=15, lineType=cv2.LINE_AA)
+    cv2.putText(whiteblankimage, text='Fix', org=(300,750),
+                fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
+                thickness=15, lineType=cv2.LINE_AA)
+    cv2.putText(whiteblankimage, text='Images', org=(300,950),
+                fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale=5, color=(0,0,0),
+                thickness=15, lineType=cv2.LINE_AA)
 
-#make image to array
-mask=np.array(whiteblankimage)
-mask_save=Image.fromarray(mask*255)
-mask_save=mask_save.convert("L")
-mask_save.save("mask_text.png")
+    #make image to array
+    mask=np.array(whiteblankimage)
+    mask_save=Image.fromarray(mask*255)
+    mask_save=mask_save.convert("L")
+    mask_save.save("{}_mask_text.png".format(number))
 
 
-#overlay mask
-masked=ar*mask
-masked_again_imimage = Image.fromarray(masked)
+    #overlay mask
+    masked=ar*mask
+    masked_again_imimage = Image.fromarray(masked)
 
-#save image with mask overlayied on it
-masked_again_imimage_save=masked_again_imimage.convert("L")
-masked_again_imimage_save.save("masked_with_text.png")
-#masked_again_imimage.show()
+    #save image with mask overlayied on it
+    masked_again_imimage_save=masked_again_imimage.convert("L")
+    masked_again_imimage_save.save("masked_with_text.png")
+    return masked, mask
 
-#D and h values
-D = .5
-h = 1
+#generating the mas kand overlayingit on top of 
+masked,mask=add_mask_of_text("Piotr",ar,1)
 
-#get position of the mask
-positions=[]
-print("Retrive the position of the mask")
-for i in  tqdm(range(len(mask))):
-    for j in range(len(mask[i])):
-        if mask[i][j]==0:
-            positions.append((i,j))
 
-#run the numerical method
-print("Run the numerical method")
-len_mask=len(mask)
-len_mask_i=len(mask[0])
-for k in tqdm(range(1000)):
-    
-    for p in positions:
-        i=p[0]
-        j=p[1]
-        if k==0:
-            n=0
+def restore_image(masked,mask,number,iteration):
+    #D and h values
+    D = .5
+    h = 1
 
-            u_0__1 = masked[i][j+1]
-            u_0_1 = masked[i][j-1]
+    #get position of the mask
+    positions=[]
+    print("Retrive the position of the mask")
+    for i in  tqdm(range(len(mask))):
+        for j in range(len(mask[i])):
+            if mask[i][j]==0:
+                positions.append((i,j))
 
-            u_1_0 = masked[i-1][j]
-            u__1_0 = masked[i+1][j]
-            
-            if not i+1 >= len_mask_i and not j+1 >= len_mask:
-                u__1_0=0
-                u_0__1=0
-                n=2
-
-            if j+1 >= len_mask and not i+1 >= len_mask_i:
-                u_0__1=0
-                n=1
-
-            if not j+1 >= len_mask and i+1 >= len_mask_i:
-                u__1_0=0
-
-                n=1
-            else:
+    #run the numerical method
+    print("Run the numerical method")
+    len_mask=len(mask)
+    len_mask_i=len(mask[0])
+    for k in tqdm(range(iteration)):
+        
+        for p in positions:
+            i=p[0]
+            j=p[1]
+            if k==0:
                 n=0
 
+                u_0__1 = masked[i][j+1]
+                u_0_1 = masked[i][j-1]
 
-            masked[i][j] =masked[i][j]+ D*h*(u_1_0+u__1_0+u_0_1+u_0__1-(4-n)*masked[i][j])
-            
-        else:
-            masked[i][j] = masked[i][j] + D*h*(masked[i-1][j]+masked[i+1][j]+masked[i][j-1]+masked[i][j+1] - 4*masked[i][j])
-
-
-#save image of the restored image
-restored = Image.fromarray(masked)
-#restored.show()           
-restored2=restored.convert("L")
-restored2.save("restored.png")
+                u_1_0 = masked[i-1][j]
+                u__1_0 = masked[i+1][j]
                 
+                if not i+1 >= len_mask_i and not j+1 >= len_mask:
+                    u__1_0=0
+                    u_0__1=0
+                    n=2
+
+                if j+1 >= len_mask and not i+1 >= len_mask_i:
+                    u_0__1=0
+                    n=1
+
+                if not j+1 >= len_mask and i+1 >= len_mask_i:
+                    u__1_0=0
+
+                    n=1
+                else:
+                    n=0
+
+
+                masked[i][j] =masked[i][j]+ D*h*(u_1_0+u__1_0+u_0_1+u_0__1-(4-n)*masked[i][j])
+                
+            else:
+                masked[i][j] = masked[i][j] + D*h*(masked[i-1][j]+masked[i+1][j]+masked[i][j-1]+masked[i][j+1] - 4*masked[i][j])
+
+
+    #save image of the restored image
+    restored = Image.fromarray(masked)
+    #restored.show()           
+    restored2=restored.convert("L")
+    restored2.save("{}_restored.png".format(number))
+    return restored
+
+#running the numerical method
+restored=restore_image(masked,mask,1,100)
 
 #calculate the error
 def error_measure(mask, original, restored):
@@ -157,5 +167,6 @@ def error_measure(mask, original, restored):
         Chi_squared_list.append((original[p[0]][p[1]]-restored[p[0]][p[1]])**2)
     Chi_squared = sum(Chi_squared_list)*1/len(positions)/sigma_squared
     #print(Chi_squared)
+
 print("Calculate the error")
 error_measure(mask, ar, masked)
